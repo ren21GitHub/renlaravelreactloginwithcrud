526eb46d7905ff51ddfc5d66d6a95eb53760375a
ren21GitHub <reynielobut21@gmail.com>
2023-12-16 18:06:57 +0800

    v1.0

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router-dom";
+
+export default function DefaultLayout(){
+    return(
+        <>
+            <h2>DefaultLayout</h2>
+        </>
+    )
+}
---
+++ react/src/views/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h2>NotFound</h2>
+        </div>
+    )
+}
