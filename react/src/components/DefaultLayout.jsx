7ad7b3728c58574880a6bcf20d96a022324d25a4
ren21GitHub <reynielobut21@gmail.com>
2023-12-19 12:27:40 +0800

    Login Signup Layout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
 
 export default function DefaultLayout(){
@@ -7,7 +7,37 @@ export default function DefaultLayout(){
     if(!token) {
         return <Navigate to="/login" />
     }
+
+    const onLogout = (ev) => {
+        ev.preventDefault()
+    }
+
     return(
-        <Outlet/>        
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">Users</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>
+                        Header
+                    </div>
+
+                    <div>
+                        {user.name} &nbsp; &nbsp;
+                        <a onClick={onLogout} className="btn-logout" href="#">Logout</a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet/>
+                </main>
+                {/* {notification &&
+                    <div className="notification">
+                        {notification}
+                    </div>
+                } */}
+            </div>
+        </div>
     )
 }
